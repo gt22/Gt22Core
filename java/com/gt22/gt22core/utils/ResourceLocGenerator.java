0494c507561a1fa54a13aaa866639b0862cb1bfe
gt22 <igorengel@mail.ru>
2016-08-14 15:00:41 +0300

    Теперь есть возможность добовлять постфиксы

--- java/com/gt22/gt22core/utils/ResourceLocGenerator.java
+++ java/com/gt22/gt22core/utils/ResourceLocGenerator.java
@@ -5,11 +5,17 @@ import com.gt22.gt22core.interfaces.IMod;
 
 public class ResourceLocGenerator
 {
-	private String modid, defoultpath;
-	public ResourceLocGenerator(String modid, String defoutpath)
+	private String modid, prefix, postfix;
+	public ResourceLocGenerator(String modid, String prefix, String postfix)
 	{
 		this.modid = modid;
-		this.defoultpath = defoutpath;
+		this.prefix = prefix;
+		this.postfix = postfix;
+	}
+	
+	public ResourceLocGenerator(String modid, String prefix)
+	{
+		this(modid, prefix, "");
 	}
 	
 	public ResourceLocGenerator(String modid)
@@ -17,18 +23,23 @@ public class ResourceLocGenerator
 		this(modid, "");
 	}
 	
-	public ResourceLocGenerator(IMod mod)
+	public ResourceLocGenerator(IMod mod, String prefix, String postfix)
 	{
-		this(mod.getModid());
+		this(mod.getModid(), prefix, postfix);
 	}
 	
-	public ResourceLocGenerator(IMod mod, String defoultpath)
+	public ResourceLocGenerator(IMod mod, String prefix)
 	{
-		this(mod.getModid(), defoultpath);
+		this(mod, prefix, "");
+	}
+	
+	public ResourceLocGenerator(IMod mod)
+	{
+		this(mod, "");
 	}
 	
 	public ResourceLocation generate(String path)
 	{
-		return new ResourceLocation(modid, defoultpath + path);
+		return new ResourceLocation(modid, prefix + path + postfix);
 	}
 }
