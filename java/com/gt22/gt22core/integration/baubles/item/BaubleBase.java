5226a340656a779876306f9c3dd25f60477a13c7
gt22 <igorengel@mail.ru>
2016-09-18 03:44:25 +0300

    Oops

--- java/com/gt22/gt22core/integration/baubles/item/BaubleBase.java
+++ java/com/gt22/gt22core/integration/baubles/item/BaubleBase.java
@@ -4,7 +4,6 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import baubles.api.BaubleType;
 import baubles.api.IBauble;
-
 import com.gt22.gt22core.baseclasses.item.ItemBase;
 import com.gt22.gt22core.interfaces.IMod;
 
@@ -15,22 +14,39 @@ public abstract class BaubleBase extends ItemBase implements IBauble
 	 * @param type of bauble
 	 * for other params see {@link ItemBase}
 	 */
-	public BaubleBase(String unlocName, IMod mod, int creativetabid, BaubleType type)
+	public BaubleBase(String unlocName, IMod mod, int creativetabid, String textureDir, BaubleType type)
 	{
-		super(unlocName, mod, creativetabid);
+		super(unlocName, mod, creativetabid, textureDir);
 		this.type = type;
 		setMaxStackSize(1);
 	}
 	
 	/**
-	 * @param type of bauble
-	 * for other params see {@link ItemBase}
+	 * See {@link #BaubleBase(String, IMod, int, String, BaubleType)}
+	 * ID defaulted to 0
+	 */
+	public BaubleBase(String unlocName, IMod mod, String textureDir, BaubleType type)
+	{
+		this(unlocName, mod, 0, textureDir, type);
+	}
+	
+	/**
+	 * See {@link #BaubleBase(String, IMod, int, String, BaubleType))}
+	 * Dir defaulted to empty (assets/modid/textures/items)
+	 */
+	public BaubleBase(String unlocName, IMod mod, int creativetabid, BaubleType type)
+	{
+		this(unlocName, mod, creativetabid, "", type);
+	}
+	
+	/**
+	 * See {@link #BaubleBase(String, IMod, int, BaubleType)}
+	 * ID defaulted to 0 
 	 */
 	public BaubleBase(String unlocName, IMod mod, BaubleType type)
 	{
 		this(unlocName, mod, 0, type);
 	}
-	
 	
 	
 	@Override
